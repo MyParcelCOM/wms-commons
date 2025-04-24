b74a9a743de68794da41c246c1dd96501f5a5391
Daniel Verner <daniel@myparcel.com>
2025-04-24 14:23:15 +0200

    Added ReturnResponse.php class

---
+++ src/Returns/Http/Responses/ReturnResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelCom\Wms\Returns\Http\Responses;
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Http\JsonResponse;
+readonly class ReturnResponse implements Responsable
+{
+    /**
+     * @param string $wmsReturnId The unique identifier assigned to the return by the Warehouse Management System (WMS).
+     */
+    public function __construct(
+        private string $wmsReturnId
+    ) {
+    }
+
+    public function toResponse($request): JsonResponse
+    {
+        return new JsonResponse([
+           'data' => [
+               'wms_return_identifier' => $this->wmsReturnId,
+           ]
+        ]);
+    }
+}
