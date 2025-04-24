fc3bcbe489e7ff70269e6737b2a7a1196f5a814c
Daniel Verner <daniel@myparcel.com>
2025-04-24 15:11:42 +0200

    Made class not read-only

--- src/Returns/Http/Responses/ReturnResponse.php
+++ src/Returns/Http/Responses/ReturnResponse.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace MyParcelCom\Wms\Returns\Http\Responses;
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\JsonResponse;
-readonly class ReturnResponse implements Responsable
+class ReturnResponse implements Responsable
 {
     /**
      * @param string $wmsReturnId The unique identifier assigned to the return by the Warehouse Management System (WMS).
      */
     public function __construct(
-        private string $wmsReturnId
+        private readonly string $wmsReturnId
     ) {
     }
 
