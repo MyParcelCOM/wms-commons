3993d939a61c92ae181baa52303b0844636d9241
yoeriwalstra <yoeri@myparcel.com>
2025-09-23 14:39:46 +0200

    :sparkles: add followUpWithConfiguration constructor property to ShopSetupResponse

--- src/Shop/Http/Responses/ShopSetupResponse.php
+++ src/Shop/Http/Responses/ShopSetupResponse.php
@@ -6,26 +6,29 @@ namespace MyParcelCom\Wms\Shop\Http\Responses;
 
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Response;
-use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class ShopSetupResponse implements Responsable
 {
+    /**
+     * @param string|null $authorizationUrl  URL to external authorization service.
+     * @param bool        $showConfiguration Instruction to the recipient of the response to fetch the configuration schema.
+     */
     public function __construct(
         private readonly ?string $authorizationUrl = null,
+        private readonly bool $showConfiguration = false,
     ) {
     }
 
-    public function toResponse($request): SymfonyResponse
+    public function toResponse($request): JsonResponse
     {
-        if (!$this->authorizationUrl) {
-            return new Response('', SymfonyResponse::HTTP_NO_CONTENT);
+        $responseData = [
+            'show_configuration' => $this->showConfiguration,
+        ];
+
+        if ($this->authorizationUrl) {
+            $responseData['authorization_url'] = $this->authorizationUrl;
         }
 
-        return new JsonResponse([
-            'data' => [
-                'authorization_url' => $this->authorizationUrl,
-            ],
-        ]);
+        return new JsonResponse(['data' => $responseData]);
     }
 }
